2c0ea6b22e468b2f8fafcb730c78c067c0c88cdd
Mateus Cerqueira <mateuscgonzaga@gmail.com>
2025-03-27 15:34:35 -0300

    Style: Estilização da página de cadastro

--- frontend/beelist/src/App.jsx
+++ frontend/beelist/src/App.jsx
@@ -3,10 +3,13 @@ import Login from './components/Login';
 
 const App = () => {
   return (
-    <div>
-      <h1>Bem-vindo ao Beelist!</h1>
+    <center>
+    <div id="bemvindo">
+    <img src="/src/img/abelha.png" alt="Logo Beelist" width="300" />
+      <h1 id="BIENVENIDO"><span id="abe">Bem-</span>vindo <span id="abe">a</span>o <span id="abe">Bee</span>list!</h1>
       <Login />
     </div>
+    </center>
   );
 };
 
